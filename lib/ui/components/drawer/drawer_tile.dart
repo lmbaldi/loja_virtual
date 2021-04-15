f7ad3b5fe753c04103e127c55b92d670b5ad6919
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-04-14 21:32:17 -0300

    navigation with provider

--- lib/ui/components/drawer/drawer_tile.dart
+++ lib/ui/components/drawer/drawer_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/ui/page_manager/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class DrawerTile extends StatelessWidget {
   final IconData iconData;
@@ -9,9 +11,12 @@ class DrawerTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final int currentPage = context.watch<PageManager>().page;
+
     return InkWell(
       onTap: () {
-
+        context.read<PageManager>().setPage(page);
       },
       child: Row(
         children: [
@@ -20,14 +25,14 @@ class DrawerTile extends StatelessWidget {
             child: Icon(
               iconData,
               size: 32,
-              color: Colors.grey[700],
+              color: currentPage == page ? Colors.red : Colors.grey[700],
             ),
           ),
           Text(
             title,
             style: TextStyle(
               fontSize: 16,
-              color: Colors.grey[700]
+              color: currentPage == page ? Colors.red : Colors.grey[700],
             ),
           )
         ],
--- lib/ui/screen/screen_base.dart
+++ lib/ui/screen/screen_base.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:loja_virtual/ui/page_manager/page_manager.dart';
 import 'package:loja_virtual/ui/components/drawer/drawer.dart';
 
 class ScreenBase extends StatelessWidget {
@@ -7,20 +9,23 @@ class ScreenBase extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      children: [
-        Scaffold(
-          drawer: DrawerCustom(),
-          appBar: AppBar(
-            title: const Text('Home'),
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          Scaffold(
+            drawer: DrawerCustom(),
+            appBar: AppBar(
+              title: const Text('Home'),
+            ),
           ),
-        ),
-        Container(color: Colors.red,),
-        Container(color: Colors.green,),
-        Container(color: Colors.blue,),
-      ],
+          Container(color: Colors.red,),
+          Container(color: Colors.green,),
+          Container(color: Colors.blue,),
+        ],
+      ),
     );
   }
 }
