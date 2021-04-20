4db6331dfdce784f91b5c38a36f62ea6f596dfe0
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-04-20 14:59:19 -0300

    blocking fields during login

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,19 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:loja_virtual/helpers/helpers.dart';
 import 'models.dart';
 
-class UserManager{
+class UserManager extends ChangeNotifier {
 
   final FirebaseAuth auth = FirebaseAuth.instance;
+  bool loading = false;
 
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
-
+    setLoading(true);
     try{
       final AuthResult result = await auth.signInWithEmailAndPassword(email: user.email, password: user.password);
       onSuccess();
     }on  PlatformException catch(e){
       onFail(getErrorString(e.code));
     }
+    setLoading(false);
+  }
+
+  void setLoading(bool value){
+    loading = value;
   }
 }
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -23,89 +23,107 @@ class LoginScreen extends StatelessWidget {
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(hintText: R.string.email),
-                    keyboardType: TextInputType.emailAddress,
-                    autocorrect: false,
-                    validator: (email) {
-                      if (!emailValid(email)) {
-                        return R.string.invalidEmail;
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: passwordController,
-                    decoration: InputDecoration(hintText: R.string.password),
-                    keyboardType: TextInputType.emailAddress,
-                    autocorrect: false,
-                    obscureText: true,
-                    validator: (password) {
-                      if (password.isEmpty || password.length < 6) {
-                        return R.string.invalidPassword;
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: FlatButton(
-                      onPressed: () {},
-                      padding: EdgeInsets.zero,
-                      child: Text(R.string.forgetPassword),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 44,
-                    child: RaisedButton(
-                      onPressed: () {
-                        if (formKey.currentState.validate()) {
-                          context.read<UserManager>().signIn(
-                              user: User(
-                                  email: emailController.text,
-                                  password: passwordController.text),
-                              onFail: (error) {
-                                scaffoldKey.currentState.showSnackBar(SnackBar(
-                                  backgroundColor: Colors.red[900],
-                                  content: Text(
-                                    R.string.errorLogin + error,
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ));
-                              },
-                              onSuccess: () {
-                                //TODO fechar tela de login
-                              });
-                        }
-                      },
-                      color: Theme.of(context).primaryColor,
-                      textColor: Colors.white,
-                      child: Text(
-                        R.string.enter,
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+            child: Consumer<UserManager>(
+              builder: (_, userManager, child) {
+                return ListView(
+                  padding: const EdgeInsets.all(16),
+                  shrinkWrap: true,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: emailController,
+                        enabled: !userManager.loading,
+                        decoration: InputDecoration(hintText: R.string.email),
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        validator: (email) {
+                          if (!emailValid(email)) {
+                            return R.string.invalidEmail;
+                          }
+                          return null;
+                        },
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: passwordController,
+                        enabled: !userManager.loading,
+                        decoration:
+                            InputDecoration(hintText: R.string.password),
+                        keyboardType: TextInputType.emailAddress,
+                        autocorrect: false,
+                        obscureText: true,
+                        validator: (password) {
+                          if (password.isEmpty || password.length < 6) {
+                            return R.string.invalidPassword;
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    child,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        height: 44,
+                        child: RaisedButton(
+                          onPressed: userManager.loading
+                              ? null
+                              : () {
+                                  if (formKey.currentState.validate()) {
+                                    userManager.signIn(
+                                        user: User(
+                                            email: emailController.text,
+                                            password: passwordController.text),
+                                        onFail: (error) {
+                                          scaffoldKey.currentState.showSnackBar(
+                                            SnackBar(
+                                              backgroundColor: Colors.red[900],
+                                              content: Text(
+                                                R.string.errorLogin + error,
+                                                textAlign: TextAlign.center,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                        onSuccess: () {
+                                          //TODO fechar tela de login
+                                        });
+                                  }
+                                },
+                          color: Theme.of(context).primaryColor,
+                          disabledColor:
+                              Theme.of(context).primaryColor.withAlpha(100),
+                          textColor: Colors.white,
+                          child: userManager.loading
+                              ? CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation(Colors.white),
+                                )
+                              : Text(
+                                  R.string.enter,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.zero,
+                    child: Text(R.string.forgetPassword),
                   ),
-                )
-              ],
+                ),
+              ),
             ),
           ),
         ),
