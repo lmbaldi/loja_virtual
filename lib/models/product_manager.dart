326c4e880c17357c126618170a8eeda9ce089a69
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-04-23 15:15:04 -0300

    searching for products by name

--- lib/models/product_manager.dart
+++ lib/models/product_manager.dart
@@ -11,6 +11,27 @@ class ProductManager extends ChangeNotifier {
 
   final Firestore firestore = Firestore.instance;
   List<Product> allProducts = [];
+  String _search = '';
+
+  set search(String value){
+    _search = value;
+    notifyListeners();
+  }
+
+  String get search => _search;
+
+  List<Product> get filteredProducts {
+    final List<Product> filteredProducts = [];
+
+    if(search.isEmpty){
+      filteredProducts.addAll(allProducts);
+    } else {
+      filteredProducts.addAll(
+        allProducts.where((p) => p.name.toLowerCase().contains(search.toLowerCase()))
+      );
+    }
+    return filteredProducts;
+  }
 
   Future<void> loadAllProducts() async {
     final QuerySnapshot snapProducts = await firestore.collection('products').getDocuments();
