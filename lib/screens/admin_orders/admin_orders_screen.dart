5974bb66bf6a294151ebc38cdfcd48801e782d3e
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-05-16 18:31:28 -0300

    displaying the status panel

--- lib/screens/admin_orders/admin_orders_screen.dart
+++ lib/screens/admin_orders/admin_orders_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 import '../../common/common.dart';
 import '../../data/data.dart';
 import '../../helpers/helpers.dart';
 
 class AdminOrdersScreen extends StatelessWidget {
+
+  final PanelController panelController = PanelController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,48 +21,96 @@ class AdminOrdersScreen extends StatelessWidget {
         builder: (_, ordersManager, __) {
           final filteredOrders = ordersManager.filteredOrders;
 
-          return Column(children: <Widget>[
-            if (ordersManager.userFilter != null)
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 0, 16, 2),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        'Pedidos de ${ordersManager.userFilter.name}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          color: Colors.white,
+          return SlidingUpPanel(
+            controller: panelController,
+            body: Column(children: <Widget>[
+              if (ordersManager.userFilter != null)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 2),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          'Pedidos de ${ordersManager.userFilter.name}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    CustomIconButton(
-                      iconData: Icons.close,
-                      color: Colors.white,
-                      onTap: () {
-                        ordersManager.setUserFilter(null);
-                      },
-                    )
-                  ],
+                      CustomIconButton(
+                        iconData: Icons.close,
+                        color: Colors.white,
+                        onTap: () {
+                          ordersManager.setUserFilter(null);
+                        },
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            if (filteredOrders.isEmpty)
-              EmptyCard(
-                title: R.string.noSalesMade,
-                iconData: Icons.border_clear,
-              )
-            else
-              Expanded(
-                child: ListView.builder(
-                    itemCount: filteredOrders.length,
-                    itemBuilder: (_, index) {
-                      return OrderTile(
-                        ordersManager.filteredOrders[index],
-                        showControls: true,
+              if (filteredOrders.isEmpty)
+                EmptyCard(
+                  title: R.string.noSalesMade,
+                  iconData: Icons.border_clear,
+                )
+              else
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: filteredOrders.length,
+                      itemBuilder: (_, index) {
+                        return OrderTile(
+                          ordersManager.filteredOrders[index],
+                          showControls: true,
+                        );
+                      }),
+                )
+            ]),
+            minHeight: 40,
+            maxHeight: 250,
+            panel: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: (){
+                    if(panelController.isPanelClosed){
+                      panelController.open();
+                    } else {
+                      panelController.close();
+                    }
+                  },
+                  child: Container(
+                    height: 40,
+                    color: Colors.white,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Filtros',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: Status.values.map((s){
+                      return CheckboxListTile(
+                        title: Text(Order.getStatusText(s)),
+                        dense: true,
+                        activeColor: Theme.of(context).primaryColor,
+                        onChanged: (v){
+
+                        },
                       );
-                    }),
-              )
-          ]);
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          );
+
         },
       ),
     );
