a91608ccb89118b0f4f53d5ce8780e3b83d9541e
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-05-21 20:36:44 -0300

    creating the store object

--- lib/data/manager/stores_manager.dart
+++ lib/data/manager/stores_manager.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../data.dart';
+
 class StoresManager extends ChangeNotifier{
 
   StoresManager(){
     _loadStoreList();
   }
 
+  List<Store> stores = [];
   final Firestore firestore = Firestore.instance;
 
   Future<void> _loadStoreList() async {
     final snapshot = await firestore.collection('stores').getDocuments();
+    stores = snapshot.documents.map((e) => Store.fromDocument(e)).toList();
     notifyListeners();
   }
 
