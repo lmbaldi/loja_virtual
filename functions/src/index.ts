43828a960c051c0a70473883cc83e239aedee226
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-05-27 20:39:04 -0300

    identifying the types of messages from service

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,12 @@
 /* eslint-disable space-before-blocks */
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { CieloConstructor, Cielo, TransactionCreditCardRequestModel, CaptureRequestModel, CancelTransactionRequestModel, EnumBrands } from "cielo";
+import { CieloConstructor, Cielo, TransactionCreditCardRequestModel, EnumBrands } from "cielo";
 
 admin.initializeApp(functions.config().firebase);
 
-const merchantId = functions.config().cielo.merchantId;
-const merchantKey = functions.config().cielo.merchantKey;
+const merchantId = functions.config().cielo.merchantid;
+const merchantKey = functions.config().cielo.merchantkey;
 
 const cieloParams: CieloConstructor = {
   merchantId: merchantId,
@@ -45,7 +45,7 @@ export const authorizeCreditCard = functions.https.onCall(async (data, context) 
 
   const userId = context.auth.uid;
   const snapshot = await admin.firestore().collection("users").doc(userId).get();
-  const userData = snapshot.data;
+  const userData = snapshot.data() || {};
   console.log("Inciando autorizacao");
 
   let brand: EnumBrands;
@@ -82,8 +82,97 @@ export const authorizeCreditCard = functions.https.onCall(async (data, context) 
           "message": "Cartão não suportado: " + data.creditCard.brand,
         },
       };
+  }
+
+  const saleData: TransactionCreditCardRequestModel = {
+    merchantOrderId: data.merchantOrderId,
+    customer: {
+      name: userData.name,
+      identity: data.cpf,
+      identityType: "CPF",
+      email: userData.email,
+      deliveryAddress: {
+        street: userData.address.street,
+        number: userData.address.number,
+        complement: userData.address.complement,
+        zipCode: userData.address.zipCode.replace(".", "").replace("-", ""),
+        city: userData.address.city,
+        state: userData.address.state,
+        country: "BRA",
+        district: userData.address.district,
+      },
+    },
+    payment: {
+      currency: "BRL",
+      country: "BRA",
+      amount: data.amount,
+      installments: data.installments,
+      softDescriptor: data.softDescriptor.substring(0, 13),
+      type: data.paymentType,
+      capture: false,
+      creditCard: {
+        cardNumber: data.creditCard.cardNumber,
+        holder: data.creditCard.holder,
+        expirationDate: data.creditCard.expirationDate,
+        securityCode: data.creditCard.securityCode,
+        brand: brand,
+      },
+    },
+  };
+  try {
+    const transaction = await cielo.creditCard.transaction(saleData);
+
+    if (transaction.payment.status === 1) {
+      return {
+        "success": true,
+        "paymentId": transaction.payment.paymentId,
+      };
+    } else {
+      let message = "";
+      switch (transaction.payment.returnCode) {
+        case "5":
+          message = "Não Autorizada";
+          break;
+        case "57":
+          message = "Cartão expirado";
+          break;
+        case "78":
+          message = "Cartão bloqueado";
+          break;
+        case "99":
+          message = "Timeout";
+          break;
+        case "77":
+          message = "Cartão cancelado";
+          break;
+        case "70":
+          message = "Problemas com o Cartão de Crédito";
+          break;
+        default:
+          message = transaction.payment.returnMessage;
+          break;
+      }
+      return {
+        "success": false,
+        "status": transaction.payment.status,
+        "error": {
+          "code": transaction.payment.returnCode,
+          "message": message,
+        },
+      };
     }
-  });
+  } catch (error) {
+    console.log("Error ", error);
+    return {
+      "success": false,
+      "error": {
+        "code": error.response[0].Code,
+        "message": error.response[0].Message,
+      },
+    };
+  }
+});
+
 
 export const helloWorld = functions.https.onCall((data, context) => {
   functions.logger.info("onCall!", { structuredData: true });
