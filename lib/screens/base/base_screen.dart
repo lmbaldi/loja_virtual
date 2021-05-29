d7c1a41badd23c8cc096f964f3e21fd339074ed0
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-05-29 16:50:43 -0300

    displaying notification of order status change

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,10 +1,13 @@
+import 'dart:io';
+
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import '../screens.dart';
 import '../../data/data.dart';
 import '../helpers/helpers.dart';
-import '../../common/common.dart';
 import '../products/products.dart';
 
 class BaseScreen extends StatefulWidget {
@@ -20,6 +23,45 @@ class _BaseScreenState extends State<BaseScreen> {
   void initState() {
     super.initState();
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    configFCM();
+  }
+
+  void configFCM(){
+    final fcm = FirebaseMessaging();
+
+    if(Platform.isIOS){
+      fcm.requestNotificationPermissions(
+          const IosNotificationSettings(provisional: true)
+      );
+    }
+
+    fcm.configure(
+        onLaunch: (Map<String, dynamic> message) async {
+          print('onLaunch $message');
+        },
+        onResume: (Map<String, dynamic> message) async {
+          print('onResume $message');
+        },
+        onMessage: (Map<String, dynamic> message) async {
+          showNotification(
+            message['notification']['title'] as String,
+            message['notification']['body'] as String,
+          );
+        }
+    );
+  }
+
+  void showNotification(String title, String message){
+    Flushbar(
+      title: title,
+      message: message,
+      flushbarPosition: FlushbarPosition.TOP,
+      flushbarStyle: FlushbarStyle.GROUNDED,
+      isDismissible: true,
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: const Duration(seconds: 5),
+      icon: Icon(Icons.shopping_cart, color: Colors.white,),
+    ).show(context);
   }
 
   @override
